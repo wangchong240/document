7e38a13e83d41ab3ac61c5bb9b21c332f4ab491f
王冲 <1032788001@qq.com>
2019-08-30 08:42:14 +0800

    file

---
+++ shell/order_rest_start.sh
@@ -0,0 +1,15 @@
+NAME='crm-rest-order'  
+F_PATH='/home/crm3user/order-rest'
+echo $NAME  
+ID=`ps -ef | grep "$NAME" | grep -v "grep" | awk '{print $2}'`  
+echo $ID  
+echo "---------------"  
+for id in $ID  
+do  
+kill -9 $ID
+echo "killed $ID"  
+done  
+echo "------killed $NAME---------"  
+
+echo "------start $NAME ---------"  
+nohup java -jar "$F_PATH"/"$NAME".jar >"$F_PATH"/nohup.out &
